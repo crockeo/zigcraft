22cfe0fbdfeded7459a49b0a8ae63543012bd75c
Cerek Hillen <cerekh@gmail.com>
2022-07-04 19:27:18 -0700

    make build.zig compile sdl2

--- build.zig
+++ build.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
-pub fn build(b: *std.build.Builder) void {
+pub fn build(b: *std.build.Builder) !void {
     const target = b.standardTargetOptions(.{});
     const mode = b.standardReleaseOptions();
     const exe = b.addExecutable("cppcraft", "src/main.zig");
@@ -8,6 +8,8 @@ pub fn build(b: *std.build.Builder) void {
     exe.setTarget(target);
     exe.setBuildMode(mode);
     exe.install();
+
+    try buildSdl2(b, exe);
 
     const run_cmd = exe.run();
     run_cmd.step.dependOn(b.getInstallStep());
@@ -156,73 +158,87 @@ fn buildBx(_: *std.build.Builder) !void {
 // )
 }
 
-fn buildSdl2(_: *std.build.Builder) !void {
-// # TODO: actually build it ourselves
-// # so that we have a hermetic build
-// cc_library(
-//     name = "sdl2",
-//     visibility = ["//visibility:public"],
-//     hdrs = glob(["include/SDL2/*.h"]),
-//     includes = ["include"],
-//     linkopts = [
-//         "-L/opt/homebrew/Cellar/sdl2/2.0.20/lib/",
-// 	"-lSDL2",
-//     ],
-// )
+fn buildSdl2(_: *std.build.Builder, exe: *std.build.LibExeObjStep) !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
 
-// cc_library(
-//     name = "sdl2_core",
-//     visibility = ["//visibility:public"],
-//     srcs = glob([
-//         "src/*.c",
-//         "src/atomic/*.c",
-//         "src/audio/*.c",
-//         "src/core/*.c",
-//         "src/cpuinfo/*.c",
-//         "src/dynapi/*.c",
-//         "src/events/*.c",
-//         "src/file/*.c",
-//         "src/filesystem/*.c",
-//         "src/haptic/*.c",
-//         "src/hidapi/*.c",
-//         "src/joystick/*.c",
-//         "src/libm/*.c",
-//         "src/loadso/*.c",
-//         "src/locale/*.c",
-//         "src/main/*.c",
-//         "src/misc/*.c",
-//         "src/power/*.c",
-//         "src/render/*.c",
-//         "src/sensor/*.c",
-//         "src/stdlib/*.c",
-//         "src/test/*.c",
-//         "src/thread/*.c",
-//         "src/timer/*.c",
-//         "src/video/*.c",
-//     ]),
-//     hdrs = glob([
-//         "src/**/*.h",
-// 	"include/**/*.h",
-//     ]),
-//     includes = [
-//         "include",
-//     ],
-// )
+    const pkg_config = try PkgConfig.init(gpa.allocator(), "sdl2");
+    defer pkg_config.deinit();
 
-// objc_library(
-//     name = "sdl2_macos_mixin",
-//     srcs = [
-//     ],
-// )
-
-// cc_library(
-//     name = "sdl2_macos",
-//     visibility = ["//visibility:public"],
-//     srcs = glob([
-//     ]),
-//     deps = [
-//         ":sdl2_core",
-// 	":sdl2_macos_mixin",
-//     ],
-// )
+    try pkg_config.addToExe(exe);
 }
+
+// The linkSystemlibrary implementation of pkg-config is incomplete
+// because it does not provide include directories.
+// So I made my own! Because that's how I am! :)
+const PkgConfig = struct {
+    allocator: std.mem.Allocator,
+    stdout: []u8,
+    include_dirs: std.ArrayList([]const u8),
+    link_dirs: std.ArrayList([]const u8),
+    link_targets: std.ArrayList([]const u8),
+
+    pub fn init(allocator: std.mem.Allocator, pkg_name: []const u8) !PkgConfig {
+        const result = try std.ChildProcess.exec(
+            .{
+                .allocator = allocator,
+                .argv = &[_][]const u8{
+                    "pkg-config",
+                    "--cflags-only-I",
+                    "--libs-only-L",
+                    "--libs-only-l",
+                    pkg_name,
+                },
+            },
+        );
+        allocator.free(result.stderr);
+
+        var include_dirs = std.ArrayList([]const u8).init(allocator);
+        var link_dirs = std.ArrayList([]const u8).init(allocator);
+        var link_targets = std.ArrayList([]const u8).init(allocator);
+
+        var iter = std.mem.split(u8, std.mem.trim(u8, result.stdout, " \n"), " ");
+        while (iter.next()) |arg| {
+            const flag = arg[0..2];
+            const content = arg[2..];
+
+            if (std.mem.eql(u8, flag, "-I")) {
+                try include_dirs.append(content);
+            } else if (std.mem.eql(u8, flag, "-L")) {
+                try link_dirs.append(content);
+            } else if (std.mem.eql(u8, flag, "-l")) {
+                try link_targets.append(content);
+            } else {
+                std.log.err("Linker flag and content: {s}{s}", .{flag, content});
+                return error.UnexpectedLinkerCommand;
+            }
+        }
+
+        return @This(){
+            .allocator = allocator,
+            .stdout = result.stdout,
+            .include_dirs = include_dirs,
+            .link_dirs = link_dirs,
+            .link_targets = link_targets,
+        };
+    }
+
+    pub fn deinit(self: *const PkgConfig) void {
+        self.allocator.free(self.stdout);
+        self.include_dirs.deinit();
+        self.link_dirs.deinit();
+        self.link_targets.deinit();
+    }
+
+    pub fn addToExe(self: *const PkgConfig, exe: *std.build.LibExeObjStep) !void {
+        for (self.include_dirs.items) |include_dir| {
+            exe.addIncludeDir(include_dir);
+        }
+        for (self.link_dirs.items) |link_dir| {
+            exe.addLibPath(link_dir);
+        }
+        for (self.link_targets.items) |link_target| {
+            exe.linkSystemLibraryName(link_target);
+        }
+    }
+};
--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,11 @@
 const std = @import("std");
 
 pub fn main() !void {
+    _ = c.SDL_Init(c.SDL_INIT_VIDEO);
     std.log.debug("hello world!", .{});
 }
+
+pub const c = @cImport({
+    @cInclude("SDL.h");
+    @cInclude("SDL_syswm.h");
+});
