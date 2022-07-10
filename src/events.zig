bac98115730ccbaa6d60b6d867a4bdb2d20a3b0b
Cerek Hillen <cerekh@gmail.com>
2022-07-10 10:41:37 -0700

    handling keyboard input

--- src/events.zig
+++ src/events.zig
@@ -1,7 +1,10 @@
+const std = @import("std");
+
 const c = @import("./bridge.zig").c;
 
 pub const EventHandler = struct {
     event: c.SDL_Event,
+    input: InputState,
     should_quit: bool,
     window_width: u32,
     window_height: u32,
@@ -9,6 +12,7 @@ pub const EventHandler = struct {
     pub fn init(initial_window_width: u32, initial_window_height: u32) EventHandler {
         return EventHandler{
             .event = undefined,
+            .input = InputState.init(),
             .should_quit = false,
             .window_width = initial_window_width,
             .window_height = initial_window_height,
@@ -21,13 +25,19 @@ pub const EventHandler = struct {
             return false;
         }
 
-        if (self.event.type == c.SDL_QUIT) {
+        if (self.event.type == c.SDL_KEYDOWN or self.event.type == c.SDL_KEYUP) {
+            self.handleKeyboardEvent(self.event.key);
+        } else if (self.event.type == c.SDL_QUIT) {
             self.should_quit = true;
         } else if (self.event.type == c.SDL_WINDOWEVENT) {
             self.handleWindowEvent(self.event.window);
         }
 
         return true;
+    }
+
+    fn handleKeyboardEvent(self: *EventHandler, event: c.SDL_KeyboardEvent) void {
+        self.input.setPressed(event.keysym.scancode, event.state == c.SDL_PRESSED);
     }
 
     fn handleWindowEvent(self: *EventHandler, event: c.SDL_WindowEvent) void {
@@ -41,5 +51,24 @@ pub const EventHandler = struct {
                 c.BGFX_TEXTURE_FORMAT_COUNT,
             );
         }
+    }
+};
+
+pub const InputState = struct {
+    state: [512]bool,
+
+    pub fn init() InputState {
+        return InputState{
+            .state = std.mem.zeroes([512]bool),
+        };
+    }
+
+    // TODO: optimize by turning this into a bitmask (if it doesn't compiler optimize already)
+    pub fn setPressed(self: *InputState, scancode: c.SDL_Scancode, pressed: bool) void {
+        self.state[scancode] = pressed;
+    }
+
+    pub fn isPressed(self: *const InputState, scancode: c.SDL_Scancode) bool {
+        return self.state[scancode];
     }
 };
